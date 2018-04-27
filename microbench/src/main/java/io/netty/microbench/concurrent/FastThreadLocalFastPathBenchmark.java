ec067fd392897fa1174d95696cc4b4b8b36f1668
unknown <diegoelias1@gmail.com>
2018-04-27 17:39:26 +0200

    Fixing a bug in the refactoring

--- microbench/src/main/java/io/netty/microbench/concurrent/FastThreadLocalFastPathBenchmark.java
+++ microbench/src/main/java/io/netty/microbench/concurrent/FastThreadLocalFastPathBenchmark.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package ;
+package io.netty.microbench.concurrent;
 
 import io.netty.microbench.util.AbstractMicrobenchmark;
 import io.netty.util.concurrent.FastThreadLocal;
